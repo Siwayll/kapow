4a6da639d9f56ee0de6c3315322c73766c85d1f1
Siwaÿll <siwayll@doublepsi.fr>
2017-12-26 22:30:26 +0100

    style fix

--- tests/units/src/Test.php
+++ tests/units/src/Test.php
@@ -7,9 +7,20 @@ use mageekguy\atoum\mock;
 
 class Test extends atoum\spec
 {
-    public function __construct(adapter $adapter = null, annotations\extractor $annotationExtractor = null, asserter\generator $asserterGenerator = null, test\assertion\manager $assertionManager = null, \closure $reflectionClassFactory = null)
-    {
-        parent::__construct($adapter, $annotationExtractor, $asserterGenerator, $assertionManager, $reflectionClassFactory);
+    public function __construct(
+        adapter $adapter = null,
+        annotations\extractor $annotationExtractor = null,
+        asserter\generator $asserterGenerator = null,
+        test\assertion\manager $assertionManager = null,
+        \closure $reflectionClassFactory = null
+    ) {
+        parent::__construct(
+            $adapter,
+            $annotationExtractor,
+            $asserterGenerator,
+            $assertionManager,
+            $reflectionClassFactory
+        );
 
         $this->getAsserterGenerator()->addNamespace('Siwayll\Kapow\Atoum');
     }
