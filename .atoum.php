4c4f1055acf4cfdb04021b8e82f40d81cac351c2
Siwaÿll <siwayll@doublepsi.fr>
2018-02-21 11:49:35 +0100

    add coveralls config

--- .atoum.php
+++ .atoum.php
@@ -9,3 +9,27 @@ $runner
     ->addTestsFromDirectory(__DIR__ . '/tests/units/')
     ->disallowUsageOfUndefinedMethodInMock()
 ;
+
+
+$travis = getenv('TRAVIS');
+if ($travis) {
+    $script->addDefaultReport();
+    $coverallsToken = getenv('COVERALLS_REPO_TOKEN');
+    if ($coverallsToken) {
+        $coverallsReport = new reports\asynchronous\coveralls('classes', $coverallsToken);
+        $defaultFinder = $coverallsReport->getBranchFinder();
+        $coverallsReport
+            ->setBranchFinder(function() use ($defaultFinder) {
+                if (($branch = getenv('TRAVIS_BRANCH')) === false) {
+                    $branch = $defaultFinder();
+                }
+                return $branch;
+            }
+        )
+            ->setServiceName('travis-ci')
+            ->setServiceJobId(getenv('TRAVIS_JOB_ID'))
+            ->addDefaultWriter()
+            ;
+        $runner->addReport($coverallsReport);
+    }
+}
